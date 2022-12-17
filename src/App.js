6f8b152f741860b4c378f1a44d60ce9b75b26f0e
JeongChaeYoon <chaeyuun32@gmail.com>
2022-12-18 02:50:04 +0900

    map함수로 동적UI만들기

--- src/App.js
+++ src/App.js
@@ -5,36 +5,66 @@ import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [a, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬 독학"]);
-  let [따봉, 따봉변경] = useState(0);
-
+  let [글제목, 제목변경] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬 독학"]);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <button onClick = {()=>{
-        let copy = [...a];
+      {/* <button onClick = {()=>{
+        let copy = [...글제목];
         copy[0] = '여자코트추천';
-        b(copy);
+        제목변경(copy);
         }}>버튼</button>
       <div className="list">
         <h4>
-          {a[0]} <span onClick={() =>{따봉변경(따봉+1)}}>❤️</span> {따봉}
+          {글제목[0]} <span onClick={() =>{따봉변경(따봉+1)}}>❤️</span> {따봉}
         </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{a[1]}</h4>
+        <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{a[2]}</h4>
+        <h4 onClick={() =>{setModal(modal+1)}}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {
+        글제목.map((a, i)=>{
+          return (
+            <div className="list" key={i}>
+              <h4>{a} <span onClick={() =>{
+                let copy = [...따봉];
+                // copy[i] = copy[i] + 1;
+                따봉변경(copy[i])}}>
+                ❤️</span> {따봉[i]} </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal % 2 == 0 ? <Modal/> : null
+      }
+
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
 }
 
 export default App;
